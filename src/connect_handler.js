bba326ee588c3ad7a219b8f7d1642f95100fc513
Arpit Patel <ad1640@outlook.com>
2025-07-02 22:17:50 -0400

    fix: Ensure ConnectHandler establishes a connection

--- src/connect_handler.js
+++ src/connect_handler.js
@@ -1,8 +1,17 @@
 const { ssh_dispatcher } = require('./ssh_dispatcher');
 
-function ConnectHandler(device) {
+async function ConnectHandler(device) {
   const ConnectionClass = ssh_dispatcher(device.device_type);
-  return new ConnectionClass(device);
+  const connection = new ConnectionClass(device);
+
+  await connection.connect();
+
+  // Enter enable mode if a secret is provided
+  if (device.secret) {
+    await connection.enable();
+  }
+
+  return connection;
 }
 
 module.exports = {
