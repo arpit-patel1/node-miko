3768ae1c3c7eefea2f5ada84cada02ef13aea864
Arpit Patel <ad1640@outlook.com>
2025-07-02 22:12:36 -0400

    fix: Correct dispatcher function call in ConnectHandler

--- src/connect_handler.js
+++ src/connect_handler.js
@@ -1,31 +1,10 @@
-const { get_network_driver } = require('./ssh_dispatcher');
+const { ssh_dispatcher } = require('./ssh_dispatcher');
 
-async function ConnectHandler(device) {
-  const Driver = get_network_driver(device.device_type);
-  if (!Driver) {
-    throw new Error(`Unsupported device_type: ${device.device_type}`);
-  }
-
-  const connection = new Driver(device);
-  await connection.connect();
-
-  if (device.secret) {
-    await connection.enable();
-  }
-
-  return connection;
+function ConnectHandler(device) {
+  const ConnectionClass = ssh_dispatcher(device.device_type);
+  return new ConnectionClass(device);
 }
 
-async function withConnection(device, task) {
-  let connection;
-  try {
-    connection = await ConnectHandler(device);
-    await task(connection);
-  } finally {
-    if (connection && connection.loggedIn) {
-      await connection.disconnect();
-    }
-  }
-}
-
-module.exports = { ConnectHandler, withConnection };
+module.exports = {
+  ConnectHandler,
+};
